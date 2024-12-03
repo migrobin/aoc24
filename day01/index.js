f63b72586e1e006ba8dd6fcc9521877986cbc5f4
migrobin <mrobin@outlook.pt>
2024-12-03 15:43:29 +0000

    2 star

--- day01/index.js
+++ day01/index.js
@@ -9,12 +9,19 @@ input.forEach((str) => {
     set2.push(Number(str.split("  ")[1]));
 });
 
-console.log(set1.sort((a, b) => a - b));
-console.log(set2.sort((a, b) => a - b));
+set1.sort((a, b) => a - b);
+set2.sort((a, b) => a - b);
 
 let res = set1.reduce((acc, val, i) => {
-    console.log(val, set2[i]);
     return (acc += Math.abs(val - set2[i]));
 }, 0);
 
 console.log(res);
+
+let sum = 0;
+
+set1.forEach((num) => {
+    sum += set2.filter((x) => x === num).length * num;
+});
+
+console.log(sum);
