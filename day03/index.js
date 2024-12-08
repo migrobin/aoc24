92a69870b3a610a2d85bef0bac02539965ba8057
migrobin <mrobin@outlook.pt>
2024-12-08 21:08:33 +0000

    transf

--- day03/index.js
+++ day03/index.js
@@ -2,21 +2,49 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("data1", "utf-8");
 
-console.log(input);
+//console.log(input);
+let match;
 
-//const regex = /mul\(\d+,\d+\)/g;
+let dos = [];
+let donts = [];
+
+const regex2 = /do\(\)/g;
+
+const regex3 = /don't\(\)/g;
+while ((match = regex2.exec(input)) !== null) {
+    dos.push(match.index);
+}
+while ((match = regex3.exec(input)) !== null) {
+    donts.push(match.index);
+}
+
+console.log(dos, donts);
+
+function isDo(index) {
+    let closestDo = dos.reverse().find((x) => x < index);
+    if (closestDo == undefined) closestDo = 0;
+    let closestDont = donts.reverse().find((x) => x < index);
+    return closestDo > closestDont ? true : false;
+}
+
+//console.log(dos);
 const regex = /mul\((\d+),(\d+)\)/g;
 let temp = [];
 
-const multiply = input.match(regex).map((line) => {
+const test = regex.exec(input);
+
+console.log("t", test);
+const multiply = test.input.match(regex).map((line, index) => {
     temp = line.split(",");
     temp[0] = Number(temp[0].slice(4));
     temp[1] = Number(temp[1].slice(0, -1));
-    console.log(temp);
-    return temp[0] * temp[1];
+    // console.log(temp);
+    return isDo(index) ? temp[0] * temp[1] : 0;
 });
 
-console.log(multiply);
+//const res= filter.map(X=>)
+//console.log(filtered);
+//console.log(multiply);
 
 let res = multiply.reduce((curr, acc) => {
     acc += curr;
