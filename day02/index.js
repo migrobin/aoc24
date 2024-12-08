edf978541f49ea656ec9b8aaece4ac23364065db
migrobin <mrobin@outlook.pt>
2024-12-08 13:21:02 +0000

    2 star

--- day02/index.js
+++ day02/index.js
@@ -4,40 +4,31 @@ const input = readFileSync("data2", "utf-8")
     .split("\n")
     .map((x) => x.split(" ").map((x) => Number(x)));
 
-const isSafe = (report) => {
+const trySafe = (report) => {
     let inc = true;
-    let badLevel = 0;
-
-    if (report[1] - report[0] < 0) inc = false;
-    if (report[1] - report[0] === 0) {
-        if (report[2] - report[1] === 0) return false;
-        if (report[2] - report[1] < 0) inc = false;
-    }
-    let badOne = 0;
-    let j = 0;
-    for (let i = 1; i < report.length; i++) {
-        if (badLevel > 0 && badOne === i - 1) {
-            j = 1;
-        }
-
-        if (inc === true) {
-            if (report[i] - report[i - 1 - j] < 1 || report[i] - report[i - 1 - j] > 3) {
-                badLevel++;
-                badOne = i;
-                if (badLevel > 1) return false;
-            }
-        } else {
-            if (report[i] - report[i - 1 - j] > -1 || report[i] - report[i - 1 - j] < -3) {
-                badLevel++;
-                badOne = i;
-                if (badLevel > 1) return false;
+    if (report[1] < report[0]) inc = false;
+    if (report[1] === report[0]) return false;
+    {
+        for (let i = 1; i < report.length; i++) {
+            if (inc === true) {
+                if (report[i] - report[i - 1] < 1 || report[i] - report[i - 1] > 3) return false;
+            } else {
+                if (report[i] - report[i - 1] > -1 || report[i] - report[i - 1] < -3) return false;
             }
         }
     }
     return true;
 };
 
-console.log(input);
+const isSafe = (report) => {
+    if (trySafe(report)) return true;
+
+    for (let i = 0; i < report.length; i++) {
+        if (trySafe(report.filter((x, index) => index !== i))) return true;
+    }
+    return false;
+};
+
 let res = input.reduce((acc, val) => {
     if (isSafe(val)) acc++;
     return acc;
