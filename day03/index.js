1509b3499e1bbbb62a0c61186341714513812151
migrobin <mrobin@outlook.pt>
2024-12-08 19:26:21 +0000

    1 star

--- day03/index.js
+++ day03/index.js
@@ -4,10 +4,23 @@ const input = readFileSync("data1", "utf-8");
 
 console.log(input);
 
-const regex = /mul\(\d+,\d+\)/g;
-const regex = /mul\((\d+),(\d+)\)/g
+//const regex = /mul\(\d+,\d+\)/g;
+const regex = /mul\((\d+),(\d+)\)/g;
+let temp = [];
 
-const filter = input.match(regex);
+const multiply = input.match(regex).map((line) => {
+    temp = line.split(",");
+    temp[0] = Number(temp[0].slice(4));
+    temp[1] = Number(temp[1].slice(0, -1));
+    console.log(temp);
+    return temp[0] * temp[1];
+});
 
-const res= filter.map(X=>)
-console.log();
+console.log(multiply);
+
+let res = multiply.reduce((curr, acc) => {
+    acc += curr;
+    return acc;
+}, 0);
+
+console.log(res);
